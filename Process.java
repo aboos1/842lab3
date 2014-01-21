35b85004a80ec725b1767c35bd2ed5c972649140
Tianyi Huang <tyhcmu@gmail.com>
2014-01-21 18:00:52 -0500

    scratch main function

---
+++ Process.java
@@ -0,0 +1,40 @@
+package lab0;
+import java.io;
+
+public class Process {
+    public static void main(String[] args) {
+		Usage();
+        MessagePasser mpasser = new MessagePasser(args[0], args[1]);
+        BufferedReader bufreader = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+            try {
+				System.out.print("$ ");
+				String command = bufreader.readLine();
+                String[] commandArgs = command.split(" ");
+                
+				if (command.equals("q")) {
+					System.exit(1);
+				}
+				else if (commandArgs[0].equals("r")) {
+					Message amessage = mpasser.receive();
+				}
+				else if (commandArgs[0].equals("s")) {
+					if (commandArgs.length != 4) {
+						Usage();
+						continue;
+					}
+					else {
+						mpasser.send(new Message(commandArgs[1], commandArgs[2], commandArgs[3]));
+					}
+				}
+	
+			} catch (IOException e) {
+				System.err.println();
+			}
+		}
+	}
+                                
+	public static void Usage() {
+		System.out.println("Usage: ");
+	}
+}
