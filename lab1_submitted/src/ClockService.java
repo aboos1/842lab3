5bfad5e81be681a9215d632d6c7c0ed6701d25e9
unknown <yasmine@cmu.edu>
2014-02-15 19:42:49 -0500

    Removed files uploaded by mistake

--- lab1_submitted/src/ClockService.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-
-public interface ClockService
-{
-	public ArrayList<Integer> getTimeStamp();
-	public void updateTimeStamp(ArrayList<Integer> list, int src_pid, int dest_pid);
-	public String toString();
-}
